9089a298ec94c5998101b54008a7dc4e20a5afbb
indrohoffmann <69474676+indrohoffmann@users.noreply.github.com>
2022-08-17 12:07:38 +0300

    syliyng mylottery and lottohtml

--- header.js
+++ header.js
@@ -5,13 +5,20 @@ import * as firebase from "./firebase.js"
 
 var loggedInUser = document.getElementById('loggedInUser');
 var loggingOut = document.getElementById("loggingOut");
-var signInLink = document.getElementById("signInLink")
+var signInLink = document.getElementById("signInLink");
+var wrapper = document.getElementById("wrapper");
+var loadingIndicator = document.getElementById("loadingIndicator");
 
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = firebase.getAuth(firebase.app);
 
 firebase.onAuthStateChanged(auth, (user) => {
+    /* wrapper.style.display = "block";
+    loadingIndicator.style.display = "none";
+    console.log(loading)*/
+
+
     if (user) {
         // User is signed in, see docs for a list of available properties
         // https://firebase.google.com/docs/reference/js/firebase.User
@@ -22,7 +29,7 @@ firebase.onAuthStateChanged(auth, (user) => {
         loggedInUser.removeAttribute("href");
         loggingOut.style.cursor = "pointer";
         signInLink.style.display = "none";
-
+        loggingOut.style.display = "block";
 
     } else {
         // User is signed out and redirected to
@@ -31,6 +38,8 @@ firebase.onAuthStateChanged(auth, (user) => {
         signInLink.style.display = "block";
     }
 });
+
+
 
 //logging out 
 loggingOut.onclick = function (e) {
